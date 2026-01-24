0571c755d300ee29b14f42fd4e00f1217c218ad7
Divyansh Singh <divyansh1539@gmail.com>
2026-01-24 14:12:03 +0530

    my first commit

---
+++ vedic_remedies/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vedic_remedies project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vedic_remedies.settings')
+
+application = get_asgi_application()
